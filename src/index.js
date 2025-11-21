18075aa9242766cb2fb9c249bf716b429d022e1e
Madi Rei <madison.nicole.rei@gmail.com>
2025-11-21 11:31:59 -0800

    changes look and functionality to createoctopus site

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-const addCrab = () => {
-  const newCrab = document.createElement('span');
-  const crabContainer = document.querySelector('#crabContainer');
-  newCrab.textContent = '🦀';
-  crabContainer.appendChild(newCrab);
+const state = {
+  octopusCount: 0,
+};
+
+const addOctopus = () => {
+  const newOctopus = document.createElement('span');
+  const octopusContainer = document.querySelector('#octopusContainer');
+  newOctopus.textContent = '🐙';
+  octopusContainer.appendChild(newOctopus);
+
+  state.octopusCount += 1;
+  const octopusCountContainer = document.querySelector('#octopusCount');
+  octopusCountContainer.textContent = `Octopus Count: ${state.octopusCount}`;
 };
 
 const registerEventHandlers = () => {
-  const crabButton = document.querySelector('#addCrabButton');
-  crabButton.addEventListener('click', addCrab);
+  const octopusButton = document.querySelector('#addOctopusButton');
+  octopusButton.addEventListener('click', addOctopus);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
